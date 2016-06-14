6f175db714ad855ccefd124dd7bc640eaf8e0548
ericmvaughn <eric_vaughn@yahoo.com>
2016-06-14 16:24:12 -0400

    Gave the default invoke and query functions name
    The default invoke is now named transfer but the
    operation is the same.  The input is two users and
    an amount to be transfer between them.
    
    The default query function is now named user and
    returns the amount assigned to a given user.

--- blocks_cc.go
+++ blocks_cc.go
@@ -72,9 +72,8 @@ func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args
 	if function == "delete" {
 		// Deletes an entity from its state
 		return t.delete(stub, args)
-	}
 
-  if function == "new" {
+	} else if function == "new" {
 		var A string    // Entities
 		var Aval int // Asset holdings
 		var err error
@@ -96,62 +95,65 @@ func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args
 		if err != nil {
 			return nil, err
 		}
-
 		return nil, nil
+
+	} else if function == "transfer" {
+
+			var A, B string    // Entities
+			var Aval, Bval int // Asset holdings
+			var X int          // Transaction value
+			var err error
+
+			if len(args) != 3 {
+				return nil, errors.New("Incorrect number of arguments. Expecting 3")
+			}
+
+			A = args[0]
+			B = args[1]
+
+			// Get the state from the ledger
+			// TODO: will be nice to have a GetAllState call to ledger
+			Avalbytes, err := stub.GetState(A)
+			if err != nil {
+				return nil, errors.New("Failed to get state")
+			}
+			if Avalbytes == nil {
+				return nil, errors.New("Entity not found")
+			}
+			Aval, _ = strconv.Atoi(string(Avalbytes))
+
+			Bvalbytes, err := stub.GetState(B)
+			if err != nil {
+				return nil, errors.New("Failed to get state")
+			}
+			if Bvalbytes == nil {
+				return nil, errors.New("Entity not found")
+			}
+			Bval, _ = strconv.Atoi(string(Bvalbytes))
+
+			// Perform the execution
+			X, err = strconv.Atoi(args[2])
+			Aval = Aval - X
+			Bval = Bval + X
+			fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
+
+			// Write the state back to the ledger
+			err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
+			if err != nil {
+				return nil, err
+			}
+
+			err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
+			if err != nil {
+				return nil, err
+			}
+
+			return nil, nil
+
+	} else {
+
+		return nil, errors.New("Invalid query function name. Expecting \"user\"")
 	}
-
-
-
-	var A, B string    // Entities
-	var Aval, Bval int // Asset holdings
-	var X int          // Transaction value
-	var err error
-
-	if len(args) != 3 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 3")
-	}
-
-	A = args[0]
-	B = args[1]
-
-	// Get the state from the ledger
-	// TODO: will be nice to have a GetAllState call to ledger
-	Avalbytes, err := stub.GetState(A)
-	if err != nil {
-		return nil, errors.New("Failed to get state")
-	}
-	if Avalbytes == nil {
-		return nil, errors.New("Entity not found")
-	}
-	Aval, _ = strconv.Atoi(string(Avalbytes))
-
-	Bvalbytes, err := stub.GetState(B)
-	if err != nil {
-		return nil, errors.New("Failed to get state")
-	}
-	if Bvalbytes == nil {
-		return nil, errors.New("Entity not found")
-	}
-	Bval, _ = strconv.Atoi(string(Bvalbytes))
-
-	// Perform the execution
-	X, err = strconv.Atoi(args[2])
-	Aval = Aval - X
-	Bval = Bval + X
-	fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
-
-	// Write the state back to the ledger
-	err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
-	if err != nil {
-		return nil, err
-	}
-
-	err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
-	if err != nil {
-		return nil, err
-	}
-
-	return nil, nil
 }
 
 // Deletes an entity from state
@@ -211,38 +213,38 @@ func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args 
     fmt.Printf("Keys operation succeeded. marshaled JSON: %s", jsonKeys)
 		return jsonKeys, nil
 
+	} else if function == "user" {
+
+		var A string // Entities
+		var err error
+
+		if len(args) != 1 {
+			return nil, errors.New("Incorrect number of arguments. Expecting name of the person to query")
+		}
+
+		A = args[0]
+
+		// Get the state from the ledger
+		Avalbytes, err := stub.GetState(A)
+		if err != nil {
+			jsonResp := "{\"Error\":\"Failed to get state for " + A + "\"}"
+			return nil, errors.New(jsonResp)
+		}
+
+		if Avalbytes == nil {
+			jsonResp := "{\"Error\":\"Nil amount for " + A + "\"}"
+			return nil, errors.New(jsonResp)
+		}
+
+		jsonResp := "{\"Name\":\"" + A + "\",\"Amount\":\"" + string(Avalbytes) + "\"}"
+		fmt.Printf("Query Response:%s\n", jsonResp)
+		return Avalbytes, nil
+
+	} else {
+
+		return nil, errors.New("Invalid query function name. Expecting \"user\"")
 	}
 
-
-
-
-	if function != "query" {
-		return nil, errors.New("Invalid query function name. Expecting \"query\"")
-	}
-	var A string // Entities
-	var err error
-
-	if len(args) != 1 {
-		return nil, errors.New("Incorrect number of arguments. Expecting name of the person to query")
-	}
-
-	A = args[0]
-
-	// Get the state from the ledger
-	Avalbytes, err := stub.GetState(A)
-	if err != nil {
-		jsonResp := "{\"Error\":\"Failed to get state for " + A + "\"}"
-		return nil, errors.New(jsonResp)
-	}
-
-	if Avalbytes == nil {
-		jsonResp := "{\"Error\":\"Nil amount for " + A + "\"}"
-		return nil, errors.New(jsonResp)
-	}
-
-	jsonResp := "{\"Name\":\"" + A + "\",\"Amount\":\"" + string(Avalbytes) + "\"}"
-	fmt.Printf("Query Response:%s\n", jsonResp)
-	return Avalbytes, nil
 }
 
 func main() {
